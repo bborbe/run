22056d52f216d9684b6a38694fe7891a76ac0092
Benjamin Borbe <bborbe@rocketnews.de>
2021-09-17 12:17:39 +0200

    add catch panic func

---
+++ panic_test.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2021 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package run_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/bborbe/run"
+)
+
+var _ = Describe("CatchPanic", func() {
+	var fn run.Func
+	var ctx context.Context
+	var err error
+	BeforeEach(func() {
+		ctx = context.Background()
+		fn = run.CatchPanic(func(ctx context.Context) error {
+			panic("banana")
+		})
+		err = fn(ctx)
+	})
+	It("returns error", func() {
+		Expect(err).NotTo(BeNil())
+	})
+	It("contains correct error message", func() {
+		Expect(err).NotTo(BeNil())
+		Expect(err.Error()).To(Equal("catch panic: banana"))
+	})
+})
